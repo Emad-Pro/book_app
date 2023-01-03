72bdc76abaeafb2dea1b46fab95339634422a241
Emad-Pro <emadeadev@gmail.com>
2023-01-03 13:01:52 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp();
+  }
+}
